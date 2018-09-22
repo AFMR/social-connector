1cce8e9520a60c6a0b457fd6584ded5fc118feda
Eury <32747825+AFMR@users.noreply.github.com>
2018-09-22 16:23:24 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+//first route
+app.get('/', (req, res) => res.send('Hello world'));
+
+const port = 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
